32a2208c03044787dfff990250549cd5db1889a4
enestekin <enestekin_@hotmail.com>
2022-03-10 01:27:11 +0300

    adding section featured

---
+++ ShopApp.UI/Views/Shared/_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopApp.UI.Views.Shared
+{
+    public class _Layout : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ShopApp.UI/Views/Shared/_Layout2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShopApp.UI.Views.Shared
+{
+    public class _Layout2 : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
